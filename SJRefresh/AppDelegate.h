663f6ff9077f840deb5c37edc69a04062eee30d2
zhoushejun <zhoushejun@passol.local>
2016-10-08 14:00:11 +0800

    init

---
+++ SJRefresh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SJRefresh
+//
+//  Created by zhoushejun on 16/10/8.
+//  Copyright © 2016年 zhoushejun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SJRefresh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SJRefresh
+//
+//  Created by zhoushejun on 16/10/8.
+//  Copyright © 2016年 zhoushejun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
